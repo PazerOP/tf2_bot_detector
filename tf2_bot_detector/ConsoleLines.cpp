6e720b11488c5b1da45b519c6a1686199ed571e1
Matt Haynie <mrhaynie@live.com>
2020-05-24 03:34:43 -0700

    Fixed not being able to read status lines for players with newlines in their names.

--- tf2_bot_detector/ConsoleLines.cpp
+++ tf2_bot_detector/ConsoleLines.cpp
@@ -244,7 +244,7 @@ ServerStatusPlayerLine::ServerStatusPlayerLine(time_point_t timestamp, PlayerSta
 
 std::unique_ptr<IConsoleLine> ServerStatusPlayerLine::TryParse(const std::string_view& text, time_point_t timestamp)
 {
-	static const std::regex s_Regex(R"regex(#\s+(\d+)\s+"(.*)"\s+(\[.*\])\s+(?:(\d+):)?(\d+):(\d+)\s+(\d+)\s+(\d+)\s+(\w+)(?:\s+(\S+))?)regex", std::regex::optimize);
+	static const std::regex s_Regex(R"regex(#\s+(\d+)\s+"(?:.|\n)"\s+(\[.*\])\s+(?:(\d+):)?(\d+):(\d+)\s+(\d+)\s+(\d+)\s+(\w+)(?:\s+(\S+))?)regex", std::regex::optimize);
 
 	if (svmatch result; std::regex_match(text.begin(), text.end(), result, s_Regex))
 	{
