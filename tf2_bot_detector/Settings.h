8482d45b5797986d38e4e74f4b14c49f3f777c0d
Matt Haynie <mrhaynie@live.com>
2020-05-31 12:59:33 -0700

    Fixed #25.

--- tf2_bot_detector/Settings.h
+++ tf2_bot_detector/Settings.h
@@ -89,6 +89,7 @@ namespace tf2_bot_detector
 				float m_ScoreboardExploiter[4] = { 0, 1, 1, 1 };
 				float m_ScoreboardRacist[4] = { 1, 1, 1, 1 };
 				float m_ScoreboardYou[4] = { 0, 1, 0, 1 };
+				float m_ScoreboardConnecting[4] = { 1, 1, 0, 0.5f };
 
 				float m_FriendlyTeam[4] = { 0.19704340398311615f, 0.5180000066757202f, 0.25745877623558044f, 0.5f };
 				float m_EnemyTeam[4] = { 0.8270000219345093f, 0.42039787769317627f, 0.38951700925827026f, 0.5f };
