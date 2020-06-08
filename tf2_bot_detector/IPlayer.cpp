a39d6d3fd49a4b6e58bf63593fba016fe3872756
ProjectOdium <fuckyou@pazer.youfag>
2020-06-09 02:47:40 +0930

    fixed shit code

--- tf2_bot_detector/IPlayer.cpp
+++
@@ -1,9 +0,0 @@
-#include "IPlayer.h"
-#include "WorldState.h"
-
-using namespace tf2_bot_detector;
-
-duration_t IPlayer::GetTimeSinceLastStatusUpdate() const
-{
-	return GetWorld().GetCurrentTime() - GetLastStatusUpdateTime();
-}
