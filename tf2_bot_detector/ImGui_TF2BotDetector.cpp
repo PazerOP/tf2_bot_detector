5a9647ca90f1e4bea1afcefac0a7bdeb2d18d182
Matt Haynie <mrhaynie@live.com>
2020-07-19 15:47:28 -0700

    Version bump

--- tf2_bot_detector/ImGui_TF2BotDetector.cpp
+++ tf2_bot_detector/ImGui_TF2BotDetector.cpp
@@ -91,39 +91,8 @@ void ImGui::AutoScrollBox(const char* ID, ImVec2 size, void(*contentsFn)(void* u
 		const auto maxScrollY = ImGui::GetScrollMaxY();
 		contentsFn(userData);
 
-#if 0
-		auto storage = ImGui::GetStateStorage();
-		static struct {} s_LastScrollPercentage; // Unique pointer
-		const auto lastScrollPercentageID = ImGui::GetID(&s_LastScrollPercentage);
-
-		static struct {} s_LastScrollMaxY;       // Unique pointer
-		const auto lastScrollMaxYID = ImGui::GetID(&s_LastScrollMaxY);
-		const auto lastScrollMaxY = storage->GetFloat(lastScrollMaxYID);
-
-		const auto lastScrollPercentage = storage->GetFloat(lastScrollPercentageID, 1.0f);
-		if (lastScrollPercentage >= 1.0f)
-			ImGui::SetScrollY(ImGui::GetScrollMaxY());
-
-		// Save the current scroll percentage
-		{
-			const auto cur = ImGui::GetScrollY();
-			const auto max = ImGui::GetScrollMaxY();
-
-			float curScrollPercentage = max == 0 ? 1.0f : cur / max;
-			if (lastScrollPercentage >= 1.0f && max > lastScrollMaxY)
-				curScrollPercentage = 1.0f;
-
-			storage->SetFloat(lastScrollPercentageID, curScrollPercentage);
-			storage->SetFloat(lastScrollMaxYID, max);
-		}
-#else
 		if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
 			ImGui::SetScrollHereY(1);
-#endif
-
-#if 0
-		ImGui::Text("Last scroll percentage: %f", storage->GetFloat(lastScrollPercentageID, NAN));
-#endif
 	}
 
 	ImGui::EndChild();
