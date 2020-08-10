22eb4394b79eb09f3993bc7d232187bb705f143e
Matt Haynie <mrhaynie@live.com>
2020-08-10 11:11:22 -0700

    Tweaks to vcpkg_setup.bat

--- vcpkg_setup.bat
+++ vcpkg_setup.bat
@@ -1,26 +1,33 @@
 @ECHO OFF
 
-SET VCPKG_ROOT=submodules\vcpkg
+PUSHD submodules\vcpkg
+	SET VCPKG_ROOT=%CD%
+	ECHO VCPKG_ROOT = %VCPKG_ROOT%
+POPD
 
 IF EXIST "%ProgramFiles(x86)%" (
 	SET VCPKG_DEFAULT_TRIPLET=x64-windows
 ) else (
 	SET VCPKG_DEFAULT_TRIPLET=x86-windows
 )
+ECHO VCPKG_DEFAULT_TRIPLET = %VCPKG_DEFAULT_TRIPLET%
 
 SETLOCAL
 
-ECHO Initializing vcpkg...
-	CALL %VCPKG_ROOT%\bootstrap-vcpkg.bat
-	IF %ERRORLEVEL% NEQ 0 EXIT /B
+SET VCPKG_EXE=%VCPKG_ROOT%\vcpkg.exe
+
+IF NOT EXIST "%VCPKG_EXE%" (
+	ECHO Building vcpkg...
+		CALL %VCPKG_ROOT%\bootstrap-vcpkg.bat
+		IF %ERRORLEVEL% NEQ 0 EXIT /B
+)
 
 ECHO Installing missing packages...
-	%VCPKG_ROOT%\vcpkg.exe install @vcpkg_packages.txt
+	%VCPKG_EXE% install @vcpkg_packages.txt
 	IF %ERRORLEVEL% NEQ 0 EXIT /B
 
 ECHO Upgrading existing packages...
-	%VCPKG_ROOT%\vcpkg.exe upgrade
+	%VCPKG_EXE% upgrade
 	IF %ERRORLEVEL% NEQ 0 EXIT /B
 
 ENDLOCAL
-PAUSE
