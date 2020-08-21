e31cb0d79bb67fe9cfef12144058837a20c53e98
Matt Haynie <mrhaynie@live.com>
2020-08-21 14:16:21 -0700

    Only resolve the version once

--- tf2_bot_detector/Version.base.h
+++ tf2_bot_detector/Version.base.h
@@ -12,21 +12,20 @@ namespace tf2_bot_detector
 		int m_Major;
 		int m_Minor;
 		int m_Patch;
-		int m_Preview;
+		int m_Build;
 	};
 
-	static constexpr char VERSION_STRING[] = "${CMAKE_PROJECT_VERSION}";
 	static constexpr Version VERSION =
 	{
 		.m_Major = ${CMAKE_PROJECT_VERSION_MAJOR},
 		.m_Minor = ${CMAKE_PROJECT_VERSION_MINOR},
 		.m_Patch = ${CMAKE_PROJECT_VERSION_PATCH},
-		.m_Preview = ${CMAKE_PROJECT_VERSION_TWEAK},
+		.m_Build = ${CMAKE_PROJECT_VERSION_TWEAK},
 	};
 }
 
 template<typename CharT, typename Traits>
 std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const tf2_bot_detector::Version& v)
 {
-	return os << v.m_Major << '.' << v.m_Minor << '.' << v.m_Patch << '.' << v.m_Preview;
+	return os << v.m_Major << '.' << v.m_Minor << '.' << v.m_Patch << '.' << v.m_Build;
 }
