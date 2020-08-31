6a1dbdc371bbdd2a7f5e5645e299104bea93c514
Matt Haynie <mrhaynie@live.com>
2020-08-31 13:07:01 -0700

    Restore a bit of the functionality of vcpkg_setup.bat.

--- vcpkg_setup.bat
+++ vcpkg_setup.bat
@@ -1,3 +1,14 @@
 @ECHO OFF
 
-ECHO vcpkg_setup.bat is deprecated and no longer necessary. It is recommended to remove it from your build script.
+PUSHD submodules\vcpkg
+    SET VCPKG_ROOT=%CD%
+    ECHO VCPKG_ROOT = %VCPKG_ROOT%
+POPD
+
+SET VCPKG_EXE=%VCPKG_ROOT%\vcpkg.exe
+
+IF NOT EXIST "%VCPKG_EXE%" (
+    ECHO Building vcpkg...
+        CALL %VCPKG_ROOT%\bootstrap-vcpkg.bat
+        IF %ERRORLEVEL% NEQ 0 EXIT /B
+)
