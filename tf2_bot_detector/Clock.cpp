1fae388c6d8c14da34edbb0e602ff973b72ed196
Matt Haynie <mrhaynie@live.com>
2021-01-17 13:35:52 -0800

    Move implementation of Clock to mh_stuff.

--- tf2_bot_detector/Clock.cpp
+++ tf2_bot_detector/Clock.cpp
@@ -1,38 +1,20 @@
 #include "Clock.h"
 
+#include <mh/chrono/chrono_helpers.hpp>
+
 using namespace tf2_bot_detector;
 
 tm tf2_bot_detector::ToTM(const time_point_t& ts)
 {
-	tm t{};
-	const auto ts_timet = clock_t::to_time_t(ts);
-
-#ifdef _WIN32
-	localtime_s(&t, &ts_timet);
-#else
-	if (auto tPtr = std::localtime(&ts_timet))
-		t = *tPtr;
-#endif
-
-	return t;
+	return mh::chrono::to_tm(ts, mh::chrono::time_zone::local);
 }
 
 tm tf2_bot_detector::GetLocalTM()
 {
-	tm retVal{};
-
-	auto time = std::time(nullptr);
-#ifdef _WIN32
-	localtime_s(&retVal, &time);
-#else
-	if (auto tPtr = std::localtime(&time))
-		retVal = *tPtr;
-#endif
-
-	return retVal;
+	return mh::chrono::current_tm(mh::chrono::time_zone::local);
 }
 
 time_point_t tf2_bot_detector::GetLocalTimePoint()
 {
-	return clock_t::from_time_t(std::time(nullptr));
+	return mh::chrono::current_time_point();
 }
